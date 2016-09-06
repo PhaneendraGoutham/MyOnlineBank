cf703cb876c2a2ea8854efb86e69b546fc2a90d8
Aman Sardana <aman.sardana83@gmail.com>
2016-09-05 19:37:03 -0500

    Initial Commit

---
+++ MyOnlineBankConfigServer/src/main/java/com/myonlinebank/configserver/MyOnlineBankConfigServerApplication.java
@@ -0,0 +1,12 @@
+package com.myonlinebank.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyOnlineBankConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyOnlineBankConfigServerApplication.class, args);
+	}
+}
---
+++ MyOnlineBankConfigServer/src/test/java/com/myonlinebank/configserver/MyOnlineBankConfigServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.myonlinebank.configserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MyOnlineBankConfigServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
